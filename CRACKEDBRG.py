7ba562c05dc2387d05af443f0b7997bafeffede2
HTS-EXPERT <83355496+HTS-EXPERT@users.noreply.github.com>
2021-05-02 18:36:08 +0300

    Add files via upload

---
+++ CRACKEDBRG.py
@@ -0,0 +1,54 @@
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+os.system('mkdir anggaxd/cloned.txt')
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+chk(): 
+  uuid = str(os.geteuid()) + str(os.getlogin()) 
+  id = "-".join(uuid) 
+  print("\n\n\x1b[37;1mYour ID : "+id) 
+  try: 
+    httpCaht = requests.get(https://raw.githubusercontent.com/HTS-EXPERT/Active/main/Active_Fast_1.txt").text 
+    if id in httpCaht: 
+      print("\033[92mYOUR ID IS ACTIVE .......\033[97m") 
+      msg = str(os.geteuid()) 
+      time.sleep(1) 
+      pass 
+    else: 
+      print("\x1b[91mYOUR ID IS NOT ACTIVE..@i4m_EXPERT.......\033[97m") 
+      time.sleep(1) 
+      sys.exit() 
+  except: 
+    sys.exit() 
+    if name == '__main__': 
+     print logo 
+     chk() 
+    
+chk()
+
+
+
+
+
+    
+
+
+
+
+
+            
